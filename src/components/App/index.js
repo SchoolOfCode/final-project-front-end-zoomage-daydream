45a205a584e1a3d950c204405260d097b33021c6
cazmaar <kazeem.alatishe@yahoo.com>
2022-02-18 12:36:07 +0000

    Structure created

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,15 +1,12 @@
 import Show from "../Show";
 
-import logo from "./logo.svg";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Show />
-      </header>
+  
     </div>
   );
 }
