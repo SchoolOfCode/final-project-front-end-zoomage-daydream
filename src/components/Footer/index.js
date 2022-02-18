a9a45afd5e91cd2267066c6b4ba83bafd18efa56
Ayomide Ojelade <ojeladea@outlook.com>
2022-02-18 15:32:15 +0000

    mail icon

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,21 +1,20 @@
 import React from "react";
-import { SocialIcon } from 'react-social-icons';
+import { SocialIcon } from "react-social-icons";
 
 const Footer = () => {
-   return (
-   <div> 
-    <footer>
-    <p>Contact Us</p>
-    <SocialIcon url="https://www.facebook.com/schoolofcode" />
-    <SocialIcon url = "https://www.instagram.com/theschoolofcode/?hl=en" />
-    <SocialIcon url="https://twitter.com/jaketrent" />
-    <SocialIcon url ="https://www.youtube.com/channel/UCKBzheEKcrqsaJhMV0f_Dmg"/>
-    <p>Copyright ©️ Space 2022 </p>
-    <SocialIcon url ="email"/>
-
-    </footer>
-   </div> 
-   )
-}
+  return (
+    <div>
+      <footer>
+        <p>Contact Us</p>
+        <SocialIcon url="https://www.facebook.com/schoolofcode" />
+        <SocialIcon url="https://www.instagram.com/theschoolofcode/?hl=en" />
+        <SocialIcon url="https://twitter.com/jaketrent" />
+        <SocialIcon url="https://www.youtube.com/channel/UCKBzheEKcrqsaJhMV0f_Dmg" />
+        <SocialIcon url="mailto:myemailaddress@gmail.com" />
+        <p>Copyright ©️ Space 2022 </p>
+      </footer>
+    </div>
+  );
+};
 
 export default Footer;
