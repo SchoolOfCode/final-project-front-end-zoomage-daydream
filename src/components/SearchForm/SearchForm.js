1b257fc357fbf290ec4e57a66325dc238a2088b8
Julija Stepanova <s.julija@me.com>
2022-02-22 11:23:28 +0000

    SearchForm removed from Display commponent

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,45 +1,45 @@
-// import React, { useState } from "react";
+import React, { useState } from "react";
 
-// const SearchForm =(props) => {
-//   const [enteredDate, setEnteredDate] = useState("");
+const SearchForm =(props) => {
+  const [enteredDate, setEnteredDate] = useState("");
   
-//   const dateChangeHandler = (event) => {
-//     setEnteredDate(event.target.value);
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
     
-//     const submitHandler = (event) => {
-//     event.preventDefault();
+    const submitHandler = (event) => {
+    event.preventDefault();
   
-//     const Date = {
-//         date: new Date(enteredDate),
-//       };
+    const Date = {
+        date: new Date(enteredDate),
+      };
   
-//     props.onSaveData(Date);
-//       setEnteredDate("");
-//     };
-//   return (
+    props.onSaveData(Date);
+      setEnteredDate("");
+    };
+  return (
     
     
     
-//     <form onSubmit={submitHandler}>
-//    <div className="search">
-//      <label>Date</label>
-//      <input
-//        type="date"
-//        min="2019-01-01"
-//        max="2022-12-31"
-//        value={enteredDate}
-//        onChange={dateChangeHandler}
-//      />
-//       <div className="button">
-//         <button type="button" onClick={props.onCancel}>
-//           Cancel
-//         </button>
-//         <button type="submit">Add Date</button>
-//       </div>
-//    </div>
-//    </form>
-//   )
-//   }
-// }
+    <form onSubmit={submitHandler}>
+   <div className="search">
+     <label>Date</label>
+     <input
+       type="date"
+       min="2019-01-01"
+       max="2022-12-31"
+       value={enteredDate}
+       onChange={dateChangeHandler}
+     />
+      <div className="button">
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button type="submit">Add Date</button>
+      </div>
+   </div>
+   </form>
+  )
+  }
+}
 
-// export default SearchForm;
+export default SearchForm;
