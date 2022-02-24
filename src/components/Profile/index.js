f33f2aa739e5148ebf96d6bfe3322caf3d565a62
Ayomide Ojelade <ojeladea@outlook.com>
2022-02-24 15:28:50 +0000

    profile icon

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,8 +1,14 @@
 import React from "react";
 import css from "./Profile.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faUserCircle,
+  
+} from "@fortawesome/free-solid-svg-icons";
 function Profile() {
   return (
     <div className={css.profileInformation}>
+    <FontAwesomeIcon icon={faUserCircle}/>
       <h2>Username</h2>
       <h2>Name</h2>
       <h2>email</h2>
@@ -10,3 +16,4 @@ function Profile() {
   );
 }
 export default Profile;
+<div>
