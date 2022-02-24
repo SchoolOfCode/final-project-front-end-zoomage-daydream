7f5ca56237e52447b4d94e2ae785d959946c5786
fatima-hashi <fmhashi91@gmail.com>
2022-02-24 12:01:24 +0000

    display gridd for sidebar cc added

--- src/Pages/Dashboard/index.js
+++ src/Pages/Dashboard/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import css from "./dashboard.module.css";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
+import Sidebar from "../../components/Sidebar";
 
 function Dashboard() {
   return (
     <div>
       <Header />
+      <Sidebar />
       <Footer />
     </div>
   );
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,8 +3,8 @@ import css from "./Sidebar.module.css";
 function Sidebar() {
     return (
         <div className={css.sidebarContainer}>
-            <h2>Username</h2>
-            <h2>Home</h2>
+            <h2 className={css.sidebarButtons}>Username</h2>
+            <h2 className={css.sidebarButtons}>Home</h2>
             <h2>Messages</h2>
             <h2>My Bookings</h2>
             <h2>Favourites</h2>
