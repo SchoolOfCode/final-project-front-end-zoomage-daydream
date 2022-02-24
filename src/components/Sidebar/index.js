625152b97d14dc40955983066ca98add05238446
fatima-hashi <fmhashi91@gmail.com>
2022-02-24 11:32:13 +0000

    sideBAR skeleton

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,2 +1,16 @@
-function Sidebar()
+import css from "./Sidebar.module.css";
+
+function Sidebar() {
+    return (
+        <div className={css.sidebarContainer}>
+            <h2>Username</h2>
+            <h2>Home</h2>
+            <h2>Messages</h2>
+            <h2>My Bookings</h2>
+            <h2>Favourites</h2>
+            <h2>Settings</h2>
+            <h2>Log Out</h2>
+        </div>
+    )
+}
 export default Sidebar
