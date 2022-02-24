3125317b232ee74660e78602599c4c97c56ff172
Ayomide Ojelade <ojeladea@outlook.com>
2022-02-24 15:29:18 +0000

    corrected div error

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,14 +1,11 @@
 import React from "react";
 import css from "./Profile.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faUserCircle,
-  
-} from "@fortawesome/free-solid-svg-icons";
+import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
 function Profile() {
   return (
     <div className={css.profileInformation}>
-    <FontAwesomeIcon icon={faUserCircle}/>
+      <FontAwesomeIcon icon={faUserCircle} />
       <h2>Username</h2>
       <h2>Name</h2>
       <h2>email</h2>
@@ -16,4 +13,3 @@ function Profile() {
   );
 }
 export default Profile;
-<div>
