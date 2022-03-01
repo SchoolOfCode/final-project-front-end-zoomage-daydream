3372f1902bf0dd6fdcf522543d848e8b4160999f
Ayomide Ojelade <ojeladea@outlook.com>
2022-03-01 11:59:29 +0000

    address input component

--- src/Pages/PropertyDetails/index.js
+++ src/Pages/PropertyDetails/index.js
@@ -1,20 +1,16 @@
-import React from 'react'
-import Header from '../../components/Header'
-import Footer from '../../components/Footer'
-
-
-
-
-
-
+import React from "react";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import AddressInput from "../../components/AddressInput";
 
 function PropertyDetails() {
   return (
     <div>
-      <Header/>
-      <Footer/>
+      <Header />
+      <AddressInput />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default PropertyDetails
+export default PropertyDetails;
