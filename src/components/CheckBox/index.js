4e59a5433330f554a9ed91706cf3b0cfe92ede36
RobertHJones <roberthuwjones@outlook.com>
2022-03-02 11:54:15 +0000

    auth0 enr variables

--- src/components/CheckBox/index.js
+++ src/components/CheckBox/index.js
@@ -1,32 +1,30 @@
-import css from "./CheckBox.module.css"
+import css from "./checkbox.module.css";
 
 const CheckBox = () => {
-    return (
-      <div className={css.amenitiesContainer}>
-      <div className={css.amenitiesTitle}> 
-      Amenities
-      </div>
-        <label> 
-          <input type="checkbox" />
-          WiFi
-        </label>
-        <label>
+  return (
+    <div className={css.amenitiesContainer}>
+      <div className={css.amenitiesTitle}>Amenities</div>
+      <label>
         <input type="checkbox" />
-          Shower
-        </label>
-        <label>
+        WiFi
+      </label>
+      <label>
         <input type="checkbox" />
-          Microwave
-        </label>
-        <label>
+        Shower
+      </label>
+      <label>
         <input type="checkbox" />
-          Standing Desk
-        </label>
-        <label>
+        Microwave
+      </label>
+      <label>
         <input type="checkbox" />
-          Fridge
-        </label>
-       </div>
-    );
-  };
-  export default CheckBox;
+        Standing Desk
+      </label>
+      <label>
+        <input type="checkbox" />
+        Fridge
+      </label>
+    </div>
+  );
+};
+export default CheckBox;
