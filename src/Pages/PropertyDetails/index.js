8da3c4a3e013eb3747431f9d9ab22693545beeb2
Ayomide Ojelade <ojeladea@outlook.com>
2022-03-02 18:53:58 +0000

    spaces drop down component

--- src/Pages/PropertyDetails/index.js
+++ src/Pages/PropertyDetails/index.js
@@ -5,6 +5,7 @@ import AddressInput from "../../components/AddressInput";
 import AdditionalInfo from "../../components/AdditionalInfo";
 import css from "./propertyDetailsForm.module.css";
 import CheckBox from "../../components/CheckBox";
+import SpacesDropDown from "../../components/SpacesDropDown";
 
 function PropertyDetails() {
   return (
@@ -13,56 +14,9 @@ function PropertyDetails() {
 
       <CheckBox />
       <AdditionalInfo />
+      <SpacesDropDown />
       <form className={css.propertyDetailsForm}>
         <AddressInput />
-
-        <div className={css.propertyDetailsDiv}>
-          <label> Type of Space:</label>
-          <br />
-
-          <select
-            name="type_of_space"
-            id="spaces"
-            placeholder="Pick a Space"
-            required
-          >
-            <option value="" disabled selected>
-              Select your option
-            </option>
-            <option value="flat">House</option>
-            <option value="house">Apartment/Flat</option>
-            <option value="mercedes">Other</option>
-            <option value="audi"></option>
-          </select>
-        </div>
-        <div className={css.propertyDetailsDiv}>
-          <label> Purpose of space:</label>
-          <br />
-
-          <select name="type_of_space" id="spaces" required>
-            <option value="" disabled selected>
-              Select your option
-            </option>
-            <option value="flat">Remote Working</option>
-            <option value="house">Music Space</option>
-            <option value="mercedes">Gym Space</option>
-            <option value="audi">Other</option>
-          </select>
-        </div>
-        <div className={css.propertyDetailsDiv}>
-          <label> Types of Spaces:</label>
-          <br />
-
-          <select name="type_of_space" id="spaces" required>
-            <option value="" disabled selected>
-              Select your option
-            </option>
-            <option value="flat">Whole Property</option>
-            <option value="house">Private Room</option>
-            <option value="mercedes">Duplex Condo</option>
-            <option value="audi">Conference Room</option>
-          </select>
-        </div>
       </form>
 
       <Footer />
