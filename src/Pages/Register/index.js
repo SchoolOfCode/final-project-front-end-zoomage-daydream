37e202b953fc941ac1bcb656478f2a36e98a8918
cazmaar <kazeem.alatishe@yahoo.com>
2022-03-07 15:09:36 +0000

    w

--- src/Pages/Register/index.js
+++ src/Pages/Register/index.js
@@ -15,11 +15,7 @@ const Register = () => {
     handleSubmit,
     formState: { errors }
   } = useForm();
-<<<<<<< HEAD
   const navigate = useNavigate();
-=======
-  // const navigate = useNavigate();
->>>>>>> RegisterHost
   const [data1, setData1] = useState();
 
   const onSubmit = async (userDetails) => {
@@ -43,91 +39,88 @@ const Register = () => {
     setData1(data);
   };
 
-<<<<<<< HEAD
   navigate("/");
-=======
-  //  navigate("/");
->>>>>>> RegisterHost
+
 
   return (
     <div className={css.registerSect}>
       <Header />
-      <h1>Register with us</h1>
-      <div className={css.mainContainer}>
-        <form className={css.inputs} onSubmit={handleSubmit(onSubmit)}>
-          <div className={css.category}>
-            <label className={css.labels}> FIRST NAME </label>
-            <br></br>
-            <input
-              type="text"
-              placeholder="First Name"
-              {...register("firstName")}
-              className={css.form}
-              required
-            />
-          </div>
-          <div className={css.category}>
-            <label className={css.labels}> LAST NAME </label>
-            <br></br>
-            <input
-              type="text"
-              placeholder="Your Surname"
-              {...register("surname")}
-              className={css.form}
-              required
-            />
-          </div>
-          <div className={css.category}>
-            <label className={css.labels}> EMAIL ADDRESS </label>
-            <br></br>
-            <input
-              type="text"
-              placeholder="Email Address"
-              {...register("emailAddress")}
-              className={css.form}
-              // value={user.email}
-              required
-            />
-          </div>
-          <div className={css.category}>
-            <label className={css.labels}> DATE OF BIRTH </label>
-            <br></br>
-            <input
-              type="date"
-              placeholder="Your Date of Birth"
-              {...register("dateOfBirth")}
-              className={css.form}
-              required
-            />
-          </div>
-          <div className={css.category}>
-            <label className={css.labels}>CHOOSE YOUR USERNAME </label>
-            <br></br>
-            <input
-              type="text"
-              placeholder="Choose a Username"
-              {...register("username")}
-              className={css.form}
-              // value={user.nickname}
-              required
-            />
-            <div>
-              <input className={css.button} type="submit" />
+      <div className={css.main}>
+        <h1>Register with us</h1>
+        <div className={css.mainContainer}>
+          <form className={css.inputs} onSubmit={handleSubmit(onSubmit)}>
+            <div className={css.category}>
+              <label className={css.labels}> FIRST NAME </label>
+              <br></br>
+              <input
+                type="text"
+                placeholder="First Name"
+                {...register("firstName")}
+                className={css.form}
+                required
+              />
             </div>
+            <div className={css.category}>
+              <label className={css.labels}> LAST NAME </label>
+              <br></br>
+              <input
+                type="text"
+                placeholder="Your Surname"
+                {...register("surname")}
+                className={css.form}
+                required
+              />
+            </div>
+            <div className={css.category}>
+              <label className={css.labels}> EMAIL ADDRESS </label>
+              <br></br>
+              <input
+                type="text"
+                placeholder="Email Address"
+                {...register("emailAddress")}
+                className={css.form}
+                // value={user.email}
+                required
+              />
+            </div>
+            <div className={css.category}>
+              <label className={css.labels}> DATE OF BIRTH </label>
+              <br></br>
+              <input
+                type="date"
+                placeholder="Your Date of Birth"
+                {...register("dateOfBirth")}
+                className={css.form}
+                required
+              />
+            </div>
+            <div className={css.category}>
+              <label className={css.labels}>CHOOSE YOUR USERNAME </label>
+              <br></br>
+              <input
+                type="text"
+                placeholder="Choose a Username"
+                {...register("username")}
+                className={css.form}
+                // value={user.nickname}
+                required
+              />
+              <div>
+                <input className={css.button} type="submit" />
+              </div>
+            </div>
+            {errors.exampleRequired && <p>This field is required</p>}
+          </form>
+          <div className={css.backgroundRegister}>
+            <img
+              className={css.img}
+              src="https://www.introtodigital.com/wp-content/uploads/2020/01/Happy-PC-user.png"
+              alt=""
+            />
           </div>
-          {errors.exampleRequired && <p>This field is required</p>}
-        </form>
-        <div className={css.backgroundRegister}>
-          <img
-            className={css.img}
-            src="https://www.introtodigital.com/wp-content/uploads/2020/01/Happy-PC-user.png"
-            alt=""
-          />
         </div>
       </div>
-      <div className={css.footer}>
-        <Footer />
-      </div>
+      <Footer />
     </div>
   );
 };
