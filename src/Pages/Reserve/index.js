5972be0af547da2e6a140bbf31846cf2be8d3656
cazmaar <kazeem.alatishe@yahoo.com>
2022-03-15 12:44:39 +0000

    d

--- src/Pages/Reserve/index.js
+++ src/Pages/Reserve/index.js
@@ -13,15 +13,16 @@ import CarouselImage from "../../components/Carousel";
 import LoginButtonReserve from "../../components/LoginReserve";
 
 const Reserve = () => {
+  const random = Math.floor(Math.random() * 15);
   const location = useLocation();
   const id = location.state;
   const [spaces] = useFetch(`${API_URL}/spaces/${id}`); //custom hook fetch
   const space = spaces[0];
-  const [users] = useFetch(`${API_URL}/users/${id}`); //custom hook fetch
+  const [users] = useFetch(`${API_URL}/users/${random}`); //custom hook fetch
   const user = users[0];
   const { isAuthenticated } = useAuth0();
 
-  console.log(user)
+  console.log(user);
 
   return (
     <div className={css.reserveContainer}>
@@ -91,7 +92,7 @@ const Reserve = () => {
           </div>
         </div>
         <div>
-          {user&&
+          {user &&
             space &&
             (isAuthenticated ? (
               <div>
